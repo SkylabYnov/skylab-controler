004f909832b20dd7f4eb1a751b910a26918643f8
AmauryRDV <97154136+AmauryRDV@users.noreply.github.com>
2025-03-19 16:33:34 +0100

    Develop data transmission (#5)
    Co-authored-by: Maxence COEUR <maxence.coeur@etu.univ-lyon1.fr>

---
+++ src/feature/buttonsManager/ButtonsManager.cpp
@@ -0,0 +1,67 @@
+#include "./ButtonsManager.h"
+#include "ButtonsManager.h"
+#include "esp_log.h"
+
+
+#define TAG "ButtonsManager"
+
+ButtonsManager::ButtonsManager(UdpServer* udpServer) {
+    this->udpServer = udpServer;
+}
+
+
+void ButtonsManager::initButton()
+{   
+    gpio_config_t io_conf = {};
+    io_conf.pin_bit_mask = (1ULL << pinButtonEmergencyStop) | (1ULL << pinButtonMotorState);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.intr_type = GPIO_INTR_NEGEDGE; 
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(0); 
+    gpio_isr_handler_add(pinButtonEmergencyStop, button_isr_handler_emergency, this);
+    gpio_isr_handler_add(pinButtonMotorState, button_isr_handler_motor, this);
+}
+
+
+void ButtonsManager::Task()
+{
+    while (true) {
+        if (buttonPressedMotorState) {
+            ControllerRequestDTO controllerRequestDTO;
+            controllerRequestDTO.buttonMotorState=new bool(true);
+            controllerRequestDTO.initCounter();
+            buttonPressedMotorState = false; 
+            cJSON* jsonObj = controllerRequestDTO.toJson();
+            char* jsonString = cJSON_PrintUnformatted(jsonObj);
+            udpServer->SendMessage(jsonString);        
+            delete jsonString;
+            cJSON_Delete(jsonObj);
+        }
+        if (buttonPressedEmergencyStop) {
+            ControllerRequestDTO controllerRequestDTO;
+            controllerRequestDTO.buttonEmergencyStop=new bool(true);
+            controllerRequestDTO.initCounter();
+            buttonPressedEmergencyStop = false; 
+            cJSON* jsonObj = controllerRequestDTO.toJson();
+            char* jsonString = cJSON_PrintUnformatted(jsonObj);
+            udpServer->SendMessage(jsonString);        
+            delete jsonString;
+            cJSON_Delete(jsonObj);
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+void IRAM_ATTR ButtonsManager::button_isr_handler_emergency(void *arg)
+{
+    ButtonsManager* self = static_cast<ButtonsManager*>(arg);
+    self->buttonPressedEmergencyStop = true;
+}
+void IRAM_ATTR ButtonsManager::button_isr_handler_motor(void *arg)
+{
+    ButtonsManager* self = static_cast<ButtonsManager*>(arg);
+    self->buttonPressedMotorState = true;
+}
--- src/feature/joysticksManager/JoysticksManager.cpp
+++ src/feature/joysticksManager/JoysticksManager.cpp
@@ -1,4 +1,6 @@
 #include "./JoysticksManager.h"
+#include "JoysticksManager.h"
+
 
 
 const char* JoysticksManager::Tag = "JoysticksManager";
@@ -8,34 +10,69 @@ JoysticksManager::JoysticksManager(UdpServer* udpServer)
 
 void JoysticksManager::Task() {
     while (true) {
-        ControllerRequestDTO controllerRequestDTO;
+        JoystickModel joystickLeft = JoystickModel(adc1_get_raw(pinJoystickLeftX),adc1_get_raw(pinJoystickLeftY));
+        JoystickModel joystickRight = JoystickModel(adc1_get_raw(pinJoystickRightX),adc1_get_raw(pinJoystickRightY));
 
-        controllerRequestDTO.joystickLeft = new JoystickModel( adc1_get_raw(pinJoystickX),
-                                                    adc1_get_raw(pinJoystickY));
+        
+        addToLastRequests(lastJoystickModelLeftTable, NBR_INCR_JOKTICK, joystickLeft);
+        addToLastRequests(lastJoystickModelRightTable, NBR_INCR_JOKTICK, joystickRight);
 
-                                                    
-        controllerRequestDTO.joystickRight = new JoystickModel( adc1_get_raw(pinJoystick2X),
-        adc1_get_raw(pinJoystick2Y));
-
-
-        if(lastController!=controllerRequestDTO){
-            lastController = controllerRequestDTO;
+        JoystickModel joystickModelLeftAverage = calculateAverageDTO(lastJoystickModelLeftTable,NBR_INCR_JOKTICK);
+        JoystickModel joystickModelRightAverage = calculateAverageDTO(lastJoystickModelRightTable,NBR_INCR_JOKTICK);
+        
+        if(lastJoystickModelLeft!=joystickModelLeftAverage || lastJoystickModelRight!=joystickModelRightAverage){
+            ControllerRequestDTO controllerRequestDTO;
+            controllerRequestDTO.ConvertJoyStickToFlightController(joystickModelLeftAverage,joystickModelRightAverage);
+            controllerRequestDTO.initCounter();
+            lastJoystickModelLeft = joystickModelLeftAverage;
+            lastJoystickModelRight = joystickModelRightAverage;
             cJSON* jsonObj = controllerRequestDTO.toJson();
             char* jsonString = cJSON_PrintUnformatted(jsonObj);
             udpServer->SendMessage(jsonString);        
             delete jsonString;
             cJSON_Delete(jsonObj);
-            
         }
 
-
-       vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(TIME_MS_BETWEEN));
+       
     }
 }
 
+void JoysticksManager::addToLastRequests(JoystickModel *list, int size, const JoystickModel &newRequest)
+{
+    // Décaler tous les éléments vers la gauche
+    for (int i = 1; i < size; i++) {
+        list[i - 1] = list[i];
+    }
+
+    // Ajouter le nouvel élément à la fin
+    list[size - 1] = newRequest;
+}
+
+JoystickModel JoysticksManager::calculateAverageDTO(const JoystickModel *list, int size) {
+    if (size == 0) return {}; // Retourne un objet vide si la liste est vide
+
+    double sumX = 0.0, sumY = 0.0;
+    int count = 0;
+
+    for (int i = 0; i < size; i++) {
+        sumX += list[i].x;
+        sumY += list[i].y;
+        count++;
+    }
+
+    // Éviter la division par zéro
+    int avgX = (count > 0) ? static_cast<int>(sumX / count) : 0;
+    int avgY = (count > 0) ? static_cast<int>(sumY / count) : 0;
+
+    return JoystickModel(avgX, avgY);
+}
+
+
 void JoysticksManager::initJoystick()
 {
-    adc1_config_channel_atten(pinJoystickX, ADC_ATTEN_DB_11);
-    adc1_config_channel_atten(pinJoystickY, ADC_ATTEN_DB_11);
-
+    adc1_config_channel_atten(pinJoystickLeftX, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(pinJoystickLeftY, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(pinJoystickRightX, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(pinJoystickRightY, ADC_ATTEN_DB_11);
 }
